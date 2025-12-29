999ed69c9bfb75eb638f7579b146d6c2bbd7772a
felixmarcellino <felixmarcellino1517@gmail.com>
2025-12-29 16:46:11 +0700

    app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import plotly.graph_objects as go
 # ----------------------------
 # Load Precomputed Dataset
 # ----------------------------
-df = pd.read_csv("Orbital_Features.csv")
+df = pd.read_csv("orbital_features.csv")
 
 # Map (n,l,m) to cluster name
 cluster_names = {0: "Compact", 1: "Intermediate", 2: "Diffuse"}
